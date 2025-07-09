768f225bd6b7a4719f166bf961e4c68b04ca49a4
zhukata <zhukata@gmail.com>
2025-07-09 13:11:56 +0300

    first step

---
+++ src/js/main.js
@@ -0,0 +1,8 @@
+// Import our custom CSS
+import '../scss/styles.scss';
+
+// Import all of Bootstrap's JS
+import * as bootstrap from 'bootstrap';
+
+import 'bootstrap/dist/css/bootstrap.min.css';
+import 'bootstrap';
